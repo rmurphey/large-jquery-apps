2cc629e86b79531c8811a603704ea46ad54d8a37
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-04-26 12:06:40 -0700

    removing test stuff

--- src/js/myApp/components/services/News.js
+++ src/js/myApp/components/services/News.js
@@ -1,12 +1,11 @@
 require([ 'components/SearchService' ], function() {
-	console.log('this callback should be first');
-	// myApp.NewsService = myApp.SearchService.extend({
-	// 	service : 'search.news',
-	// 	
-	// 	_filterResults : function(results) {
-	// 		$.each(results, function(r) {
-	// 			r.title = 'News: ' + r.title;
-	// 		});
-	// 	}
-	// });	
+	myApp.NewsService = myApp.SearchService.extend({
+		service : 'search.news',
+		
+		_filterResults : function(results) {
+			$.each(results, function(r) {
+				r.title = 'News: ' + r.title;
+			});
+		}
+	});	
 });
--- src/js/myApp/pages/home.js
+++ src/js/myApp/pages/home.js
@@ -1,12 +1,16 @@
 require([
-	"components/services/News",
 	"components/SearchBox",
 	"components/SavedSearches",
 	"components/SearchResults"
 ], function() {
-	console.log('this callback should be second');
-	// new myApp.NewsService();
-	// new myApp.SearchResults($('#search_results'), {});
-	// new myApp.SavedSearches($('#saved_searches'), {});
-	// new myApp.SearchBox($('#search_box'), {});
+	new myApp.SearchResults($('#search_results'), {});
+	new myApp.SavedSearches($('#saved_searches'), {});
+	new myApp.SearchBox($('#search_box'), {});
+});
+
+require([
+	"components/SearchService",
+	"components/services/News"
+], function() {
+	new myApp.NewsService();
 });
